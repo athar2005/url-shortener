21be860285d1c593156464d564009b8552fbdb6f
Athar Shaikh <athar21sd11@gmail.com>
2026-01-02 02:11:08 +0530

    Initial URL shortener project

---
+++ backend/server.js
@@ -0,0 +1,68 @@
+const path = require("path");
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const shortid = require("shortid");
+const QRCode = require("qrcode");
+const Url = require("./models/Url");
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+// ✅ Serve frontend files
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("URL Shortener API Running");
+});
+
+// Shorten URL + QR
+app.post("/shorten", async (req, res) => {
+  try {
+    const { longUrl, customCode } = req.body;
+
+    if (!longUrl) {
+      return res.status(400).json({ error: "Long URL required" });
+    }
+
+    const shortCode = customCode || shortid.generate();
+
+    // ✅ CHECK IF CUSTOM CODE ALREADY EXISTS
+    const existing = await Url.findOne({ shortCode });
+    if (existing) {
+      return res.status(409).json({
+        error: "Custom short name already exists"
+      });
+    }
+
+    const shortUrl = `http://localhost:3000/${shortCode}`;
+    const qrCode = await QRCode.toDataURL(shortUrl);
+
+    const url = new Url({ longUrl, shortCode });
+    await url.save();
+
+    res.json({ longUrl, shortUrl, qrCode });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+// Redirect
+app.get("/:code", async (req, res) => {
+  const url = await Url.findOne({ shortCode: req.params.code });
+  if (url) {
+    return res.redirect(url.longUrl);
+  }
+  res.status(404).send("URL not found");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
